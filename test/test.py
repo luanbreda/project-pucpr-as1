79532b8369502c8111525f2ded16cf26edb183dd
luanbreda <luan0709@gmail.com>
2025-04-27 17:31:25 -0300

    Update test.py

--- test/test.py
+++ test/test.py
@@ -2,10 +2,10 @@ import pytest
 from src.funcao_util import remove_acentos
 
 def test_remove_acentos_lowercase():
-    assert remove_acentos("áéíóúãõâêîôû") == "aeiouaeiou"
+    assert remove_acentos("áéíóúãõâêîôû") == "aeiouaoaeiou"
 
 def test_remove_acentos_uppercase():
-    assert remove_acentos("ÁÉÍÓÚÃÕÂÊÎÔÛ") == "AEIOUAEIOU"
+    assert remove_acentos("ÁÉÍÓÚÃÕÂÊÎÔÛ") == "AEIOUAOAEIOU"
 
 def test_remove_acentos_special_characters():
     assert remove_acentos("ç") == "c"
