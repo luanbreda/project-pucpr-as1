43b2189f78a49e5e048b50b55e93a3024f42a7b6
luanbreda <luan0709@gmail.com>
2025-04-27 18:12:22 -0300

    altera_testes_unitarios

--- src/funcao_util.py
+++ src/funcao_util.py
@@ -11,5 +11,26 @@ def remove_acentos(texto):
         if unicodedata.category(c) != 'Mn'
     )
 
-def minha_funcao():
-    return "Teste"
+def busca_maior_nota(nota):
+    
+    if nota is None or len(nota) == 0:
+        return 'NA'
+
+    lista_nota = [x for x in nota.split('#')]
+    return max(float(lista_nota))
+
+
+def busca_media_todas_notas(materia,nota,filtro):
+    
+    if materia is None or len(materia) == 0:
+        return 'NA'
+    
+    lista_materia = [x for x in materia.split('#')]
+
+    vlr_total = 0
+    qtd_total = 0
+    for x in lista_materia:
+        if x in (filtro):
+            vlr_total += float(nota[lista_materia.index(x)])
+            qtd_total += 1
+    return vlr_total / qtd_total if qtd_total > 0 else 'NA'
